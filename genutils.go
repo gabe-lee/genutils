75902724d9b39a8666ef470f3c07d4381516942a
Gabriel Anderson <gla.ander@gmail.com>
2022-05-26 12:19:44 -0500

    add DeepError type

---
+++ genutils.go
@@ -0,0 +1,71 @@
+package genutils
+
+import (
+	"errors"
+	"strings"
+)
+
+type DeepError struct {
+	IsErr    bool
+	Text     string
+	Children []DeepError
+	Total    uint
+}
+
+func NewDeepError(text string) DeepError {
+	return DeepError{
+		IsErr:    true,
+		Text:     text,
+		Children: make([]DeepError, 0, 1),
+		Total:    1,
+	}
+}
+
+func (e *DeepError) Error() string {
+	if !e.IsErr {
+		return ""
+	}
+	builder := strings.Builder{}
+	e.BuildError(&builder, 0)
+	return builder.String()
+}
+
+func (e *DeepError) FlatError() error {
+	if !e.IsErr {
+		return nil
+	}
+	builder := strings.Builder{}
+	e.BuildError(&builder, 0)
+	return errors.New(builder.String())
+}
+
+func (e *DeepError) AddChildError(err error) {
+	if err == nil {
+		return
+	}
+	e.IsErr = true
+	e.Total += 1
+	e.Children = append(e.Children, NewDeepError(err.Error()))
+}
+
+func (e *DeepError) AddChildDeepError(err DeepError) {
+	if !err.IsErr {
+		return
+	}
+	e.IsErr = true
+	e.Total += err.Total
+	e.Children = append(e.Children, err)
+}
+
+func (e *DeepError) BuildError(builder *strings.Builder, depth int) {
+	if !e.IsErr {
+		return
+	}
+	tabs := strings.Repeat("\t", depth)
+	builder.WriteString("\n")
+	builder.WriteString(tabs)
+	builder.WriteString(e.Text)
+	for _, ec := range e.Children {
+		ec.BuildError(builder, depth+1)
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/gabe-lee/genutils
+
+go 1.18
